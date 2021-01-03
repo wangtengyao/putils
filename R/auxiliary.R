f9ab9585989dff69fb8835914571240909e37be1
Tengyao Wang <wangtengyao@gmail.com>
2021-01-03 20:02:30 +0000

    changed matplotlib_palette

--- R/auxiliary.R
+++ R/auxiliary.R
@@ -277,8 +277,7 @@ setNA <- function(v, a){
 #' @export
 matplotlib_palette <- function(n=0, scheme='default', visualise=FALSE){
   default_palette <- c("#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd",
-                       "#8c564b","#e377c2","#7f7f7f","#bcbd22","#17becf",
-                       "#c5b0d5","#c49c94","#f7b6d2","#c7c7c7","#dbdb8d","#9edae5")
+                       "#8c564b","#e377c2","#7f7f7f","#bcbd22","#17becf")
   bright_palette <- c("#0e4897","#17813f","#1d99b4","#1f9ee8","#25ca7a","#471c7c",
                       "#68c7ed","#6d4e98","#73af38","#7f1273","#9e1653","#ab0077",
                       "#b01426","#b1b2b4","#c1d430","#cc0b24","#e10064","#e12653",
@@ -295,12 +294,34 @@ matplotlib_palette <- function(n=0, scheme='default', visualise=FALSE){
     ret <- full_palette
   } else {
     reps <- ceiling(n / length(full_palette))
-    ret <- rep(full_palette, reps)[1:n]
+    ret = character()
+    for (rep in 1:reps){
+      mod_color <- unname(sapply(full_palette, function(c)mix_color(c, '#ffffff', (rep-1)/reps)))
+      if (rep==reps) mod_color <- head(mod_color, n - length(full_palette)*(reps-1))
+      ret <- c(ret, mod_color)
+    }
   }
 
   if (visualise) barplot(rep(1, length(ret)), col=ret, axes=F, border=F,
                          names.arg=seq_along(ret), cex.names=0.8)
   return(ret)
+}
+
+#' Color mixing
+#' @param color1 color 1 in hexadecimal
+#' @param color2 color 2 in hexadecimal
+#' @param lambda interpolation, if equal to 0, return color1, if 1 return color2
+mix_color <- function(color1, color2, lambda=0.5){
+  R1 <- strtoi(paste0('0x', substr(color1, 2, 3)))
+  G1 <- strtoi(paste0('0x', substr(color1, 4, 5)))
+  B1 <- strtoi(paste0('0x', substr(color1, 6, 7)))
+  R2 <- strtoi(paste0('0x', substr(color2, 2, 3)))
+  G2 <- strtoi(paste0('0x', substr(color2, 4, 5)))
+  B2 <- strtoi(paste0('0x', substr(color2, 6, 7)))
+  R <- round(R1 * (1-lambda) + R2 * lambda, 0)
+  G <- round(G1 * (1-lambda) + G2 * lambda, 0)
+  B <- round(B1 * (1-lambda) + B2 * lambda, 0)
+  return(rgb(R, G, B, maxColorValue = 255))
 }
 
 
